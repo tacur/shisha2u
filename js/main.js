c9e8d41d5f0cb939d039573c9322407bbd0a0150
tacur <36847088+tacur@users.noreply.github.com>
2019-10-21 20:58:18 +0200

    sdsd

--- js/main.js
+++ js/main.js
@@ -403,10 +403,11 @@ jQuery(document).ready(function($){
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 const links = document.querySelectorAll('.nav-links li');
+const lines = document.querySelectorAll('.line');
 
 hamburger.addEventListener("click",  () => {
 	navLinks.classList.toggle("open");
-	
+	lines.classList.toggle("open");
 });
 
 navLinks.addEventListener("click",  () => {
